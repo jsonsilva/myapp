6fd136b0b1ff6c0f21ca84f3541d9e752d600dda
991rajat <991rajat@gmail.com>
2019-06-21 06:03:52 +0530

    Initial commit

---
+++ app/src/main/java/android/example/myapp/MainActivity.java
@@ -0,0 +1,63 @@
+package android.example.myapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button btn;
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn_submit);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser()!=null)
+        {
+            Intent intent = new Intent(MainActivity.this,activity_home.class);
+            startActivity(intent);
+        }
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String editemail = email.getText().toString().trim();
+                String editpassword = password.getText().toString().trim();
+                firebaseAuth.signInWithEmailAndPassword(editemail,editpassword).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Intent intent = new Intent(MainActivity.this,activity_home.class);
+                            startActivity(intent);
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this,"NO",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
