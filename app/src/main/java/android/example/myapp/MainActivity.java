8bbc3fe2ac0dcf5829c16d423588c737b20e7f59
991rajat <991rajat@gmail.com>
2019-06-22 07:56:21 +0530

    comment

--- app/src/main/java/android/example/myapp/MainActivity.java
+++ app/src/main/java/android/example/myapp/MainActivity.java
@@ -15,6 +15,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+
+
+//ITS FORM rajat end
 public class MainActivity extends AppCompatActivity {
 
     EditText email,password;
